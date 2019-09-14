a8b75b861c8ee7190758ff0ec9f5f5ab107cf453
isaiah <isaiahballah@gmail.com>
2019-09-14 03:17:28 -0400

    text bash skeleton

--- api.js
+++ api.js
@@ -6,20 +6,32 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-
 app.get("/", (req, res) => {
 	res.send("cool story bro");
-})
+});
 
 const displaySms = (msisdn, text) => {
     console.log('FROM: ' + msisdn);
     console.log('MESSAGE: ' + text);
     console.log('---');
 }	
+
+const batchService = require("./services/batchService");
+const messageService = require("./services/messageService");
+
 const handleInboundSms = (request, response) => {
 	const params = Object.assign(request.query, request.body);
 	displaySms(params.msisdn, params.text);
+	var command = params.text;
+	var number = params.msisdn;
 
+	function callback(stdout){
+		console.log(stdout);
+		if (stdout)
+			messageService(stdout, number);
+	}
+
+	batchService(command).then(callback);
     // Send OK status
     response.status(204).send();
 }

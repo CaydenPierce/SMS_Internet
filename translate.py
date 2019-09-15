3cee34d23213371b32b999fdf4a426ebc68af5aa
Yusuf-shaik <43193906+Yusuf-shaik@users.noreply.github.com>
2019-09-15 04:27:55 -0400

    Add files via upload
    added error handling

--- translate.py
+++ translate.py
@@ -1,20 +1,42 @@
 import sys
 import requests
+import re
 from googletrans import Translator
 from googletrans import LANGUAGES
+# 
 
-search = sys.argv[1:2]
 
-words=sys.argv[1]
+search = sys.argv[1:]
+
+if len(search)!=2:
+	print("Please enter valid inputs")
+	exit()
+# print(len(search))
+
+# print(sys.argv[1])
+
+words=re.sub(r"[^\w\s]", '',sys.argv[1])
+# print(words)
+# print(words)
 destination=sys.argv[2]
+
 
 LANGCODES = dict(map(reversed, LANGUAGES.items()))
 translator = Translator()
 
 
-print(words)
-print(destination)
-print(LANGCODES[destination])
+if (destination not in LANGCODES.keys()):
+ 	print('No Language Found, Please Clarify')
+ 	exit()
+
+# print(LANGCODES.keys())
+# regex=re.sub('[.,()0-9:;]+', '', words)
+# # print(regex)
+
+
+# LANGCODES = dict(map(reversed, LANGUAGES.items()))
+# translator = Translator()
+
 
 new=translator.translate(words, destination)
 print(new.text)
